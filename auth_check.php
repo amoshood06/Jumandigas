a3ad47f1ebce513a913fa85bbe17770761cc0e26
amoshood06 <amoshood06@gmail.com>
2025-02-24 18:22:20 +0100

    iionuiiuiw

--- auth_check.php
+++ auth_check.php
@@ -11,7 +11,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
 $allowed_pages = [
     "user" => ["index.php"],
-    "vendor" => ["index.php"],
+    "vendor" => ["index.php", "vendor-order-management.php"],
     "rider" => ["index.php"]
 ];
 
